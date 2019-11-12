f1583b708617d21a53ec3d9b885ef6f4133bb22c
cstansfield <connor_stansfield@live.com>
2019-11-12 20:04:40 +0400

    updating

--- mng box2djs tutorial/movingboxes.js
+++ mng box2djs tutorial/movingboxes.js
@@ -1,5 +1,4 @@
 (function () {
-    // Pull out common classes
     var b2Vec2 = Box2D.Common.Math.b2Vec2;
     var b2BodyDef = Box2D.Dynamics.b2BodyDef;
     var b2Body = Box2D.Dynamics.b2Body;
@@ -17,7 +16,6 @@
     var ACC_ACCELERATE = 1;
     var ACC_BRAKE = 2;
     var KEYS_DOWN = {};
-
     var BINDINGS = {
         accelerate: 38,
         brake: 40,
@@ -25,9 +23,7 @@
         steer_right: 39
     };
 
-    // API_Method 01 - Create world constructor
     var Physics = window.Physics = function (element, scale) {
-        // var gravity = new b2Vec2(0, 9.8);
         var gravity = new b2Vec2(0, 0);
         this.world = new b2World(gravity, false);
         this.element = element;
@@ -36,7 +32,7 @@
         this.dtRemaining = 0;
         this.stepAmount = 1 / 60;
     };
-    // API_Method 02 - Stepping the world
+
     Physics.prototype.step = function (dt) {
         this.dtRemaining += dt;
         while (this.dtRemaining > this.stepAmount) {
@@ -47,24 +43,22 @@
         }
         if (this.debugDraw) {
             this.world.DrawDebugData();
-
         } else {
-            // API Part 09 modify step method to take over rendering
             var obj = this.world.GetBodyList();
             this.context.clearRect(0, 0, this.element.width, this.element.height);
-
             this.context.save();
             this.context.scale(this.scale, this.scale);
             while (obj) {
                 var body = obj.GetUserData();
-                if (body) { body.draw(this.context); }
-
+                if (body) {
+                    body.draw(this.context);
+                }
                 obj = obj.GetNext();
             }
             this.context.restore();
         }
     };
-    // API_Method 03 - debug draw
+
     Physics.prototype.debug = function () {
         this.debugDraw = new b2DebugDraw();
         this.debugDraw.SetSprite(this.context);
@@ -75,32 +69,63 @@
         this.world.SetDebugDraw(this.debugDraw);
     };
 
-    // API Part 11 - Physics handle click events
     Physics.prototype.click = function (callback) {
         var self = this;
-
         function handleClick(e) {
             e.preventDefault();
             var point = {
                 x: (e.offsetX || e.layerX) / self.scale,
                 y: (e.offsetY || e.layerY) / self.scale
             };
-
             self.world.QueryPoint(function (fixture) {
                 callback(fixture.GetBody(),
                     fixture,
                     point);
             }, point);
         }
-
         this.element.addEventListener("mousedown", handleClick);
         this.element.addEventListener("touchstart", handleClick);
     };
 
     Physics.prototype.handleKeys = function (e) {
-        if(e.type === 'keydown') KEYS_DOWN[e.keyCode] = true;
-        else if(e.type === 'keyup') KEYS_DOWN[e.keyCode] = false;
+        if (e.type === 'keydown') KEYS_DOWN[e.keyCode] = true;
+        else if (e.type === 'keyup') KEYS_DOWN[e.keyCode] = false;
     }
+
+    function degrees_to_radians(degrees) {
+        var pi = Math.PI;
+        return degrees * (pi / 180);
+    }
+
+    function dotproduct(v1, v2) {
+        return (v1[0] * v2[0]) + (v1[1] * v2[1]);
+    };
+
+    function normaliseRadians(radians) {
+        radians = radians % (2 * Math.PI);
+        if (radians < 0) {
+            radians += (2 * Math.PI);
+        }
+        return radians;
+    };
+
+    function rotatevector(v, angle) {
+        angle = normaliseRadians(angle);
+        return [v[0] * Math.cos(angle) - v[1] * Math.sin(angle),
+        v[0] * Math.sin(angle) + v[1] * Math.cos(angle)];
+    };
+
+    function vector_len(v) {
+        return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+    };
+
+    function unit_vector(v) {
+        var l = vector_len(v);
+        if (l) {
+            return [v[0] / l, v[1] / l];
+        }
+        return [0, 0];
+    };
 
     function Wheel(details) {
         this.position = [details.x, details.y];
@@ -108,19 +133,20 @@
         this.revolving = details.revolving;
         this.powered = details.powered;
         this.details = details = details || {};
-
+        
         var def = new b2BodyDef();
+        for (var k in this.defaults) {
+            this.details[k] = details[k] || this.defaults[k];
+        }
         def.type = b2Body.b2_dynamicBody;
         def.position = this.car.body.GetWorldPoint(new b2Vec2(this.position[0], this.position[1]));
         def.angle = this.car.body.GetAngle();
-
         this.body = window.physics.world.CreateBody(def);
-
         var fixdef = new b2FixtureDef;
         fixdef.density = 1;
         fixdef.isSensor = true; //wheel does not participate in collision calculations: resulting complications are unnecessary
         fixdef.shape = new b2PolygonShape();
-        fixdef.shape.SetAsBox(this.details.width / 2, this.details.length / 2);
+        fixdef.shape.SetAsBox(this.details.width / 2, this.details.height / 2);
         this.body.CreateFixture(fixdef);
         //create joint to connect wheel to body
         if (this.revolving) {
@@ -136,27 +162,11 @@
         window.physics.world.CreateJoint(jointdef);
     }
 
-    function degrees_to_radians(degrees) {
-        var pi = Math.PI;
-        return degrees * (pi / 180);
-    }
-
-    function dotproduct(v1, v2) {
-        return (v1[0] * v2[0]) + (v1[1] * v2[1]);
-    };
-
-    function normaliseRadians(radians){
-        radians=radians % (2*Math.PI);
-        if(radians<0) {
-            radians+=(2*Math.PI);
-        }
-        return radians;
-    };
-
-    function rotatevector(v, angle) {
-        angle = normaliseRadians(angle);
-        return [v[0] * Math.cos(angle) - v[1] * Math.sin(angle),
-        v[0] * Math.sin(angle) + v[1] * Math.cos(angle)];
+    Wheel.prototype.defaults = {
+        shape: "block",
+        color: "black",
+        width: 1,
+        height: 1.8,
     };
 
     Wheel.prototype.setAngle = function (angle) {
@@ -184,6 +194,21 @@
         this.body.SetLinearVelocity(new b2Vec2(kv[0], kv[1]));
     };
 
+    Wheel.prototype.draw = function (context) {
+        console.log(this);
+        var pos = this.body.GetPosition(),
+            angle = this.body.GetAngle();
+        context.save();
+        context.translate(pos.x, pos.y);
+        context.rotate(angle);
+        context.fillStyle = this.details.color;
+        context.fillRect(-this.details.width / 2,
+            -this.details.height / 2,
+            this.details.width,
+            this.details.height);
+        context.restore();
+    }
+
     var Body = window.Body = function (physics, details) {
         this.details = details = details || {};
         this.definition = new b2BodyDef();
@@ -195,19 +220,15 @@
         this.definition.userData = this;
         this.definition.type = details.type == "static" ? b2Body.b2_staticBody :
             b2Body.b2_dynamicBody;
-
-        // Create the Body
         this.body = physics.world.CreateBody(this.definition);
 
-        // Create the fixture
         this.fixtureDef = new b2FixtureDef();
         for (var l in this.fixtureDefaults) {
             this.fixtureDef[l] = details[l] || this.fixtureDefaults[l];
         }
 
-        // Create the shape
         details.shape = details.shape || this.defaults.shape;
-
+        
         switch (details.shape) {
             case "circle":
                 details.radius = details.radius || this.defaults.radius;
@@ -241,88 +262,6 @@
         }
     };
 
-    var Car = window.Car = function(physics, details, carDetails){
-        Body.call(this, physics, details);
-        this.steer = STEER_NONE;
-        this.accelerate = ACC_NONE;
-        this.wheel_angle = 0;
-        this.max_steer_angle = carDetails.max_steer_angle;
-        this.max_speed = carDetails.max_speed;
-        this.power = carDetails.power;
-    }
-
-    Car.prototype = Object.create(Body.prototype);
-    Car.prototype.constructor = Car;
-
-    Car.prototype.defaults = {
-        width: 5,
-        height: 10,
-        shape: "block",
-    };
-
-    Car.prototype.fixtureDefaults = {
-        density: 1.0,
-        friction: 0.3,
-        restitution: 0.4
-    };
-
-    Car.prototype.definitionDefaults = {
-        linearDamping: 0.15,
-        bullet: true,
-        angularDamping: 0.3,
-    };
-
-    function vector_len(v) {
-        return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
-    };
-
-    function unit_vector(v) {
-        var l = vector_len(v);
-        if (l) {
-            return [v[0] / l, v[1] / l];
-        }
-        return [0, 0];
-    };
-
-    Body.prototype.getPoweredWheels = function () {
-        var retv = [];
-        for (var i = 0; i < this.wheels.length; i++) {
-            if (this.wheels[i].powered) {
-                retv.push(this.wheels[i]);
-            }
-        }
-        return retv;
-    };
-
-    Body.prototype.getLocalVelocity = function () {
-        var retv = this.body.GetLocalVector(this.body.GetLinearVelocityFromLocalPoint(new b2Vec2(0, 0)));
-        return [retv.x, retv.y];
-    };
-
-    Body.prototype.getRevolvingWheels = function () {
-        var retv = [];
-        for (var i = 0; i < this.wheels.length; i++) {
-            if (this.wheels[i].revolving) {
-                retv.push(this.wheels[i]);
-            }
-        }
-        return retv;
-    };
-
-    Body.prototype.getSpeedKMH = function () {
-        var velocity = this.body.GetLinearVelocity();
-        var len = vector_len([velocity.x, velocity.y]);
-        return (len / 1000) * 3600;
-    };
-
-    Body.prototype.setSpeed = function (speed) {
-        var velocity = this.body.GetLinearVelocity();
-        velocity = unit_vector([velocity.x, velocity.y]);
-        velocity = new b2Vec2(velocity[0] * ((speed * 1000.0) / 3600.0),
-            velocity[1] * ((speed * 1000.0) / 3600.0));
-        this.body.SetLinearVelocity(velocity);
-    };
-
     Body.prototype.defaults = {
         shape: "block",
         width: 4,
@@ -343,10 +282,143 @@
         angularVelocity: 0,
         awake: true,
         bullet: false,
-        fixedRotation: false
+        fixedRotation: false,
     };
 
-    Body.prototype.update = function (msDuration) {
+    Body.prototype.draw = function (context) {
+        var pos = this.body.GetPosition(),
+            angle = this.body.GetAngle();
+        context.save();
+        context.translate(pos.x, pos.y);
+        context.rotate(angle);
+
+        if (this.details.color) {
+            context.fillStyle = this.details.color;
+
+            switch (this.details.shape) {
+                case "circle":
+                    context.beginPath();
+                    context.arc(0, 0, this.details.radius, 0, Math.PI * 2);
+                    context.fill();
+                    break;
+                case "polygon":
+                    var points = this.details.points;
+                    context.beginPath();
+                    context.moveTo(points[0].x, points[0].y);
+                    for (var i = 1; i < points.length; i++) {
+                        context.lineTo(points[i].x, points[i].y);
+                    }
+                    context.fill();
+                    break;
+                case "block":
+                    context.fillRect(-this.details.width / 2,
+                        -this.details.height / 2,
+                        this.details.width,
+                        this.details.height);
+                default:
+                    break;
+            }
+        }
+
+        if (this.details.image) {
+            context.drawImage(this.details.image,
+                -this.details.width / 2,
+                -this.details.height / 2,
+                this.details.width,
+                this.details.height);
+        }
+        context.restore();
+    }
+
+    var Car = window.Car = function (physics, details, carDetails) {
+        Body.call(this, physics, details);
+        this.steer = STEER_NONE;
+        this.accelerate = ACC_NONE;
+        this.wheel_angle = 0;
+        this.max_steer_angle = carDetails.max_steer_angle;
+        this.max_speed = carDetails.max_speed;
+        this.power = carDetails.power;
+    }
+
+    Car.prototype = Object.create(Body.prototype);
+    Car.prototype.constructor = Car;
+    Car.prototype.draw = function (context) {
+        var pos = this.body.GetPosition(),
+            angle = this.body.GetAngle();
+        context.save();
+        context.translate(pos.x, pos.y);
+        context.rotate(angle);
+        for (i = 0; i < this.wheels.length; i++) {
+            this.wheels[i].draw(context);
+        }
+        if (this.details.image) {
+            context.drawImage(this.details.image,
+                -this.details.width / 2,
+                -this.details.height / 2,
+                this.details.width,
+                this.details.height);
+        }
+        context.restore();
+    };
+
+    Car.prototype.defaults = {
+        width: 2.5,
+        height: 5,
+        shape: "block",
+    };
+
+    Car.prototype.fixtureDefaults = {
+        density: 1.0,
+        friction: 0.3,
+        restitution: 0.4
+    };
+
+    Car.prototype.definitionDefaults = {
+        linearDamping: 0.3,
+        bullet: true,
+        angularDamping: 0.3,
+    };
+
+    Car.prototype.getPoweredWheels = function () {
+        var retv = [];
+        for (var i = 0; i < this.wheels.length; i++) {
+            if (this.wheels[i].powered) {
+                retv.push(this.wheels[i]);
+            }
+        }
+        return retv;
+    };
+
+    Car.prototype.getLocalVelocity = function () {
+        var retv = this.body.GetLocalVector(this.body.GetLinearVelocityFromLocalPoint(new b2Vec2(0, 0)));
+        return [retv.x, retv.y];
+    };
+
+    Car.prototype.getRevolvingWheels = function () {
+        var retv = [];
+        for (var i = 0; i < this.wheels.length; i++) {
+            if (this.wheels[i].revolving) {
+                retv.push(this.wheels[i]);
+            }
+        }
+        return retv;
+    };
+
+    Car.prototype.getSpeedKMH = function () {
+        var velocity = this.body.GetLinearVelocity();
+        var len = vector_len([velocity.x, velocity.y]);
+        return (len / 1000) * 3600;
+    };
+
+    Car.prototype.setSpeed = function (speed) {
+        var velocity = this.body.GetLinearVelocity();
+        velocity = unit_vector([velocity.x, velocity.y]);
+        velocity = new b2Vec2(velocity[0] * ((speed * 1000.0) / 3600.0),
+            velocity[1] * ((speed * 1000.0) / 3600.0));
+        this.body.SetLinearVelocity(velocity);
+    };
+
+    Car.prototype.update = function (msDuration) {
         //kill sideways velocity for all wheels
         var i;
         for (i = 0; i < this.wheels.length; i++) {
@@ -410,8 +482,7 @@
                     try {
                         if (body.details.player) { return body; }
                     } catch (error) {
-                        console.log(body);
-                        console.log(error);
+                        console.log(body, error);
                     }
                 }
                 obj = obj.GetNext();
@@ -441,45 +512,45 @@
     };
 
     function createWorld() {
+        var carImage = new Image();
         physics = window.physics = new Physics(document.getElementById("b2dCanvas"));
-        physics.debug();
-        new Body(physics, { color: "red", type: "static", x: 0, y: 0, height: 50, width: 0.5 });
-        new Body(physics, { color: "red", type: "static", x: 51, y: 0, height: 50, width: 0.5 });
-        new Body(physics, { color: "red", type: "static", x: 0, y: 0, height: 0.5, width: 120 });
-        new Body(physics, { color: "red", type: "static", x: 0, y: 25, height: 0.5, width: 120 });
-        new Body(physics, { color: "gray", shape: "circle", radius: 4, x: 5, y: 20 });
+        carImage.addEventListener("load", function () {
 
-        var car = new Car(physics, {
-            color: "yellow",
-            x: 25,
-            y: 10,
-            player: true,
-            wheels: [{ 'x': -2.5, 'y': -3.2, 'width': 1, 'length': 1.8, 'revolving': true, 'powered': true }, //top left
-            { 'x': 2.5, 'y': -3.2, 'width': 1, 'length': 1.8, 'revolving': true, 'powered': true }, //top right
-            { 'x': -2.5, 'y': 3.2, 'width': 1, 'length': 1.8, 'revolving': false, 'powered': false }, //back left
-            { 'x': 2.5, 'y': 3.2, 'width': 1, 'length': 1.8, 'revolving': false, 'powered': false }]//back right
-        },{
-            angle: 180,
-            power: 200,
-            max_steer_angle: 20,
-            max_speed: 200,
-            linearDamping: 0.15,
-            bullet: true,
-            angularDamping: 0.3,
-            density: 1.0,
-            friction: 0.3,
-            restitution: 0.4,  
-        });
+            // physics.debug();
+            new Body(physics, { color: "red", type: "static", x: 0, y: 0, height: 50, width: 0.5 });
+            new Body(physics, { color: "red", type: "static", x: 51, y: 0, height: 50, width: 0.5 });
+            new Body(physics, { color: "red", type: "static", x: 0, y: 0, height: 0.5, width: 120 });
+            new Body(physics, { color: "red", type: "static", x: 0, y: 25, height: 0.5, width: 120 });
+            new Body(physics, { color: "gray", shape: "circle", radius: 4, x: 5, y: 20 });
 
-        physics.click(function (body) {
-            console.log(body)
+            var car = new Car(physics, {
+                color: "yellow",
+                x: 25,
+                y: 15,
+                image: carImage,
+                player: true,
+                wheels: [{ 'x': -2.5, 'y': -3.2, 'revolving': true, 'powered': true }, //top left
+                { 'x': 2.5, 'y': -3.2, 'revolving': true, 'powered': true }, //top right
+                { 'x': -2.5, 'y': 3.2, 'revolving': false, 'powered': false }, //back left
+                { 'x': 2.5, 'y': 3.2, 'revolving': false, 'powered': false }]//back right
+            }, {
+                angle: 180,
+                power: 400,
+                max_steer_angle: 45,
+                max_speed: 200,
+            });
+
+            physics.click(function (body) {
+                console.log(body)
+            });
         });
+        carImage.src = "images/f1.png";
     }
 
     function init() {
         createWorld();
-        window.addEventListener('keydown', physics.handleKeys );
-        window.addEventListener('keyup', physics.handleKeys );
+        window.addEventListener('keydown', physics.handleKeys);
+        window.addEventListener('keyup', physics.handleKeys);
         requestAnimationFrame(gameLoop);
     }
 
